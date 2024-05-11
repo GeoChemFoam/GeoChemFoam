69edadccc10c6b674b18ab3b2f903864dd690c91
jcmaes <j.maes@hw.ac.uk>
2024-05-11 08:38:52 +0100

    EstailladesMicro600 remove

--- Examples/dispersionPaper/EstailladesMicro600/runCaseDispersion.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-###### USERS INPUT ############################################################
-
-#### END OF USER INPUT
-
-# Run simpleFoam in parallel
-echo -e "Run dispersionFoam in parallel"
-srun dispersionFoam -parallel  > dispersionFoamD.out
